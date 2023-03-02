9f7a537d4b3837eeed7473c847655843d3c770e2
Yulliana Quintana <yuliana.quintana@weareinsolite.com>
2023-03-02 19:43:33 -0300

    turial/remix

--- app/routes/posts/$slug.tsx
+++ app/routes/posts/$slug.tsx
@@ -2,7 +2,7 @@ import type { LoaderArgs } from "@remix-run/node";
 import { json } from "@remix-run/node";
 import { useLoaderData } from "@remix-run/react";
 import invariant from "tiny-invariant";
-
+import { marked } from "marked";
 import { getPost } from "~/models/post.server";
 
 export const loader = async ({ params }: LoaderArgs) => {
@@ -11,16 +11,20 @@ export const loader = async ({ params }: LoaderArgs) => {
   const post = await getPost(params.slug);
   invariant(post, `Post not found: ${params.slug}`);
 
+  const html = marked(post.markdown);
+  return json({ post, html });
+
   return json({ post });
 };
 
 export default function PostSlug() {
-  const { post } = useLoaderData<typeof loader>();
+  const { post, html } = useLoaderData<typeof loader>();
   return (
     <main className="mx-auto max-w-4xl">
       <h1 className="my-6 border-b-2 text-center text-3xl">
         {post.title}
       </h1>
+      <div dangerouslySetInnerHTML={{ __html: html }} />
     </main>
   );
 }
